8bf2049eaf039db7688f87ef5ef202290aad4e5e
WilliamDeen <williamdeentje@gmail.com>
2016-01-20 15:38:06 +0100

    Connection update 50000

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
 
-$dbCon = mysqli_connect("localhost", "root", "", "learnlearn");
+$dbCon = mysql_connect("mysql22469-devteam1.groundctrl.nl", "root", "IDAxli17288");
+    mysql_select_db("loopjijalbij");
 
 if($dbCon == false){
     echo ("Probeer opnieuw");
