17b9b1b7417f110271a1c1d95f77f108a75d282d
WilliamDeen <williamdeentje@gmail.com>
2016-01-20 15:13:27 +0100

    connection update 2

--- connection.php
+++ connection.php
@@ -1,23 +1,4 @@
 <?php
-
-	// This file can be included on every page thwer
-	// a database connection is required
-
-	// old = localhost & empty password
-
-	$servername	 = "https://mysql22469-devteam1.groundctrl.nl";
-	$username	 = "root";
-	$password	 = "IDAxli17288";
-	$dbName		 = "loopjijalbij";
-
-	$dbCon = mysqli_connect($servername, $username, $password, $dbName);
-
-	// Check connection
-	if ($dbCon->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	echo "Connected successfully";
+    $connect = mysql_connect("https://mysql22469-devteam1.groundctrl.nl", "root", "IDAxli17288");
+    mysql_select_db("loopjijalbij");
 ?>
-
-
-
