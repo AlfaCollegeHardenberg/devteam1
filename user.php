c1038bace714c07a603ed8e356a9167a4987fced
WilliamDeen <williamdeentje@gmail.com>
2016-01-21 09:45:20 +0100

    voortgang website test

--- user.php
+++ user.php
@@ -26,7 +26,7 @@ if (isset($_SESSION['id'])) {
 
 <section>
     
-    <a class="link3" href="">
+    <a class="link3" href="voortgang.html">
     	<div id="voortgangknop">
         </div>
     </a>
