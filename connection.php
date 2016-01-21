718e56062aaa9ef9ae1269b693538df7445fa582
WilliamDeen <williamdeentje@gmail.com>
2016-01-21 11:37:29 +0100

    connection.php aangepast

--- connection.php
+++ connection.php
@@ -3,7 +3,13 @@
 $dbCon = mysql_connect("mysql22469-devteam1.groundctrl.nl", "root", "IDAxli17288");
     mysql_select_db("loopjijalbij");
 
-if($dbCon == false){
-    echo ("Probeer opnieuw");
+if ($dbCon->connect_error) {
+    die("Connection failed": . $dbCon->connect_error);
 }
+echo "Connected succesfully";
+
+
+//if($dbCon == false){
+//    echo ("Probeer opnieuw");
+//}
 ?>
