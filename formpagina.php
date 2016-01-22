8b87cf1e8bcd3cbe1a0bea8dc1ddf09a421aa74c
WilliamDeen <williamdeentje@gmail.com>
2016-01-22 10:15:36 +0100

    formpagina erbij

--- formpagina.php
+++ formpagina.php
@@ -17,17 +17,38 @@
     }
 ?> 
 
+<?php
+    if (isset($_SESSION['id'])) {
+    $userId = $_SESSION['id'];
+    $username = $_SESSION['username'];
+} else {
+    header('Location: index.php');
+    die();
+}
+
+
+require_once('header.php');
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Name Storage</title>
 </head>
 <body>
+    
+    <div id="pag3blok1">
+        
     <h1>Name Storage</h1>
     <form action="formpagina.php" method="POST">
         <input type="text" name="name_first" value="" placeholder="First Name">
         <input type="text" name="name_last" value="" placeholder="Last Name">
         <input type="submit" name="submit" value="Submit">
     </form>
+        
+    </div>
 </body>
 </html>
+
+<?php
+	require_once ('footer.php');
+?>
